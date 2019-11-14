7c30c98fcc38e5269b11a5e069809f2233fd2dc9
Ratan Uday Kumar <uday@evontex.com>
2019-11-14 11:34:37 +0530

    minor db changhes and cors

--- Config/config.js
+++ Config/config.js
@@ -1,5 +1,11 @@
 let config = function () { };
-config.dbUrl = 'mongodb://appuser:appuser123@localhost:27017/connecting_app';
+config.dbUrl = 'mongodb+srv://ca:ca123@udayalldb-ddbav.mongodb.net/connectingapp';
 config.port = '3000';
 config.Admin_Code = 'E29CD-AWS3A-RF6MN-76DDA-PT45F';
+
+config.CORS_Accept_Origin = [
+    "http://localhost:4200",
+    "http://localhost:3200"
+];
+
 module.exports = config;
--- index.js
+++ index.js
@@ -6,5 +6,14 @@ let app = express();
 mongoose.connect(config.dbUrl, { useNewUrlParser: true });
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '2mb' }));
+app.use(function (req, res, next) {
+    let origin = req.headers.origin;
+    if (config.CORS_Accept_Origin.indexOf(origin) > -1) {
+        res.setHeader('Access-Control-Allow-Origin', origin);
+    }
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 app.use('/', require('./Router/router'));
 app.listen(config.port, () => console.log(`Connecting app running on ${config.port}`));
