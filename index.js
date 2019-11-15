c9a341e1b4c3471bfb5a9e6d3dd68c1731d44cef
Ratan Uday Kumar <uday@evontex.com>
2019-11-15 09:32:31 +0530

    package updated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let config = require('./Config/config');
 let bodyParser = require('body-parser');
 let mongoose = require('mongoose');
 let app = express();
-mongoose.connect(config.dbUrl, { useNewUrlParser: true });
+mongoose.connect(config.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.json({ limit: '2mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '2mb' }));
 app.use(function (req, res, next) {
